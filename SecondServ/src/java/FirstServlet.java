28ac94c81202bd36e496e98c007ace812b9cf8d0
Akash Ukarande <ukab23@gmail.com>
2018-02-08 12:50:50 +0530

    Add files via upload

---
+++ SecondServ/src/java/FirstServlet.java
@@ -0,0 +1,16 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException{
+        RequestDispatcher rd = req.getRequestDispatcher("SecondServlet");
+        rd.forward(req, res);
+    }
+}
